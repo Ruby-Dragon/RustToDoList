2151ddd64ddceaaefa1e1e9066473565f9a0df8c
Ruby-Dragon <91497228+Ruby-Dragon@users.noreply.github.com>
2022-01-31 19:11:48 -0800

    Delete rust_to_do_list/target directory

--- rust_to_do_list/target/debug/deps/rust_to_do_list-90bc36cd3ec9e718.d
+++
@@ -1,6 +0,0 @@
-/home/runner/RustToDoList/rust_to_do_list/target/debug/deps/rust_to_do_list-90bc36cd3ec9e718: src/main.rs src/args.rs
-
-/home/runner/RustToDoList/rust_to_do_list/target/debug/deps/rust_to_do_list-90bc36cd3ec9e718.d: src/main.rs src/args.rs
-
-src/main.rs:
-src/args.rs:
--- rust_to_do_list/target/debug/rust_to_do_list.d
+++
@@ -1,1 +0,0 @@
-/home/runner/RustToDoList/rust_to_do_list/target/debug/rust_to_do_list: /home/runner/RustToDoList/rust_to_do_list/src/args.rs /home/runner/RustToDoList/rust_to_do_list/src/main.rs
