0224a163510142cf37386a94a6671dd7818110b0
Ruby-Dragon <91497228+Ruby-Dragon@users.noreply.github.com>
2022-02-01 21:19:22 +0000

    clean up code

--- rust_to_do_list/src/main.rs
+++ rust_to_do_list/src/main.rs
@@ -4,13 +4,11 @@ mod task;
 use chrono;
 
 fn main() {
+		//get cli args, and remove exec name
 	  let mut args: Vec<String> = env::args().collect();
 		args.remove(0);
-		let mut cstyleint:i32 = 20;
-    println!("Hello, world! {}", cstyleint);
 
-		let result = args::return_string(23);
-		println!("{}", result);
+    println!("Hello, world!");
 
     let mut thing = task::Task {name : "urmom".to_string(), is_complete: false, date : chrono::NaiveDate::parse_from_str("2022-02-01", "%Y-%m-%d").unwrap()};
 
