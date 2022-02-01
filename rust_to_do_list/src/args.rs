99c41ae4860c37371921547a74ad9cd1acf1576e
Ruby-Dragon <91497228+Ruby-Dragon@users.noreply.github.com>
2022-02-01 03:10:20 +0000

    whoops forgor to push

---
+++ rust_to_do_list/src/args.rs
@@ -0,0 +1,10 @@
+pub fn parse_args(args : Vec<String>)
+{
+	
+}
+
+pub fn return_string(n: i32) -> String
+{
+	let f : String = "returner".to_string();
+	return f;
+}
--- rust_to_do_list/src/main.rs
+++ rust_to_do_list/src/main.rs
@@ -1,3 +1,17 @@
+use std::env;
+mod args;
+
 fn main() {
-    println!("Hello, world!");
+	  let mut args: Vec<String> = env::args().collect();
+		args.remove(0);
+		let mut cstyleint:i32 = 20;
+    println!("Hello, world! {}", cstyleint);
+
+		let result = args::return_string(23);
+		println!("{}", result);
+
+		for arg in args
+		{
+			println!("{}", arg);
+		}
 }
