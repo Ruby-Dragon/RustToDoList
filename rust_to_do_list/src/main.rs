99ff2be31fbc375bced32be14fdfb89efd613cf0
Ruby-Dragon <91497228+Ruby-Dragon@users.noreply.github.com>
2022-02-03 19:55:24 +0000

    remove some test code

--- rust_to_do_list/src/main.rs
+++ rust_to_do_list/src/main.rs
@@ -10,8 +10,7 @@ fn main() {
 	  let mut args: Vec<String> = env::args().collect();
 		args.remove(0);
 
-    println!("Hello, world!");
-
+		//all test code and will be removed
     let mut thing = task::Task {name : "urmom".to_string(), is_complete: false, date : chrono::NaiveDate::parse_from_str("2022-02-01", "%Y-%m-%d").unwrap()};
 
 		println!("{}", thing.to_string());
