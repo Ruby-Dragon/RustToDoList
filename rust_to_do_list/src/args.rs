e548b29b16a3ec1246a2a26dd96aa6df55f81943
Ruby-Dragon <91497228+Ruby-Dragon@users.noreply.github.com>
2022-02-03 19:43:41 +0000

    add default file name as const

--- rust_to_do_list/src/args.rs
+++ rust_to_do_list/src/args.rs
@@ -4,6 +4,9 @@ use crate::files::*;
 
 pub fn parse_args(args : &Vec<String>)
 {
+
+		const list_file_name : &str= "storedtodolist.lst";
+
 		if args.len() > 0{
 				//if there are any arguments
 				println!("{}", args[0]);
