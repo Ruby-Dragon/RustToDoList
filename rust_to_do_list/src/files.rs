efcfa08b187ffed9bd3bc97943caae0cbd1ce87d
Ruby-Dragon <91497228+Ruby-Dragon@users.noreply.github.com>
2022-02-03 18:35:18 +0000

    setup reading

--- rust_to_do_list/src/files.rs
+++ rust_to_do_list/src/files.rs
@@ -4,8 +4,8 @@ use std::fs::File;
 use std::io::Write;
 use std::path::Path;
 
-pub fn write_list_to_file(mut task_list : TaskList, filename : String)
-{
+pub fn write_list_to_file(mut task_list : TaskList, filename : String){
+
 		let mut file = std::fs::File::create(&filename)
 				.expect("Failed to create file.");
 
@@ -18,4 +18,18 @@ pub fn write_list_to_file(mut task_list : TaskList, filename : String)
 		file.write_all(final_file_output
 				.as_bytes())
 				.expect("Failed to write to file.");
+}
+
+pub fn read_list_from_file(filename : String) -> TaskList{
+
+		let mut file = std::fs::File::open(&filename)
+				.expect("File cannot be opened, file may not exist.");
+
+		let mut whole_file_text : String = String::new();
+
+		file
+				.read_to_string(&mut whole_file_text)
+				.expect("Error in reading data, file may be corrupted or incorrectly encoded.");
+
+		
 }
