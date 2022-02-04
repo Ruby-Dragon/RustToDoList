65fb22012aa6d0a78a8cdda49f3f9b8ce41664dc
Ruby-Dragon <91497228+Ruby-Dragon@users.noreply.github.com>
2022-02-04 21:59:33 +0000

    remove test code

--- rust_to_do_list/src/main.rs
+++ rust_to_do_list/src/main.rs
@@ -10,30 +10,12 @@ fn main() {
 	  let mut args: Vec<String> = env::args().collect();
 		args.remove(0);
 
-		//all test code and will be removed
-    let mut thing = task::Task {name : "urmom".to_string(), is_complete: false, date : chrono::NaiveDate::parse_from_str("2022-02-01", "%Y-%m-%d").unwrap()};
-
-		//println!("{}", thing.to_string());
-
-		let mut thing_list = list::TaskList {last_update_date : chrono::NaiveDate::parse_from_str("2022-02-01", "%Y-%m-%d").unwrap(), task_vec : vec![thing.clone()]};
-
-		thing_list.add(thing.clone());
-		thing_list.remove(0);
-
-		thing_list.complete_task(0);
-
-		//println!("{}", thing_list.to_string());
-
-		let mut another_list = files::read_list_from_file(args::LIST_FILE_NAME);
-
-		//println!("{}", another_list.to_string());
-
 		args::parse_args(&args);
-
-		println!(" ");
-
+		
+		/*
 		for arg in args
 		{
 			println!("{}", arg);
 		}
+		*/
 }
